c3ee67ba5a9d945e73f01ab8b84bc4485c4208ff
ShadowSpeedy7 <46542737+ShadowSpeedy7@users.noreply.github.com>
2019-01-09 22:37:29 -0600

    Initial files

---
+++ check for folder perms.ps1
@@ -0,0 +1,11 @@
+﻿$User = "$($env:computername)\test"
+$Path = "C:\users\test\desktop"
+
+$Perms = Get-Acl $Path
+$flag = 0
+ForEach ($Perm in $Perms.Access) {
+    if ($Perm.IdentityReference -eq $User -and $Perm.AccessControlType -eq "Deny" -and $Perm.FileSystemRights -eq "Write") {
+        $flag = 1
+    }
+}
+Write-Host $flag
---
+++ set folder perms.ps1
@@ -0,0 +1,23 @@
+﻿$Path = "C:\users\test\desktop"
+$User = "test"
+
+if (!$Args[0]) {
+Write-Host "No command specified, use 'ACL.ps1 lock' or 'ACL.ps1 unlock'"
+exit
+}
+
+if ($Args[0] -ne "unlock" -and $Args[0] -ne "lock") {
+    Write-Host "Unknown command argument - use 'lock' or 'unlock'"
+    exit
+}
+
+$ACL = Get-Acl $Path 
+$Perm = New-Object system.security.accesscontrol.filesystemaccessrule("$($env:computername)\$($User)","Write","Deny")
+
+if ($Args[0] -eq "lock") {
+    $ACL.SetAccessRule($Perm)
+}
+else {
+    $ACL.RemoveAccessRule($Perm) > $null
+}
+Set-Acl $Path $ACL
